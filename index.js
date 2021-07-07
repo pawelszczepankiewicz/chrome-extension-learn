8661b3e3363e8b11a96bf56119eb0da75cfbe323
pawel.szczepankiewicz <pawel.szczepankiewicz@enginiety.com>
2021-07-07 10:10:07 +0200

    Added delete button & chrome tab API

--- index.js
+++ index.js
@@ -2,30 +2,49 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
+const saveTabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
-    renderLeads();
-    inputEl.value = "";
-    inputEl.focus();
-});
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+saveTabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads);
+    })
+})
+
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
                 </a>
             </li>
         `;
-
-        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-
-        // const li = document.createElement("li");
-        // li.textContent = myLeads[i];
-        // ulEl.append(li);
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+    inputEl.focus();
+});
+
